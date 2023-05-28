0323405dfc34f62d3a47295fc5b86b6d8d04c577
Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>
2023-05-28 05:31:23 +0300

    adjustinging quotes

--- pages/checkout/[[...slug]].tsx
+++ pages/checkout/[[...slug]].tsx
@@ -1,48 +1,55 @@
-import {useCart} from '../../hooks/cart';
-import {startCheckout, StarterWrapper, resetCheckoutState} from 'boundless-checkout-react';
-import {useRouter} from 'next/router';
-import {apiClient} from '../../lib/api';
-import {TCartInited} from '../../redux/reducers/cart';
-import Loader from '../../components/Loader';
-import logoImg from '../../assets/logo.svg';
-import Head from 'next/head';
-import {useCallback, useEffect, useRef} from 'react';
-import {createGetStr} from 'boundless-api-client/utils';
+import { useCart } from "../../hooks/cart";
+import {
+	startCheckout,
+	StarterWrapper,
+	resetCheckoutState,
+} from "boundless-checkout-react";
+import { useRouter } from "next/router";
+import { apiClient } from "../../lib/api";
+import { TCartInited } from "../../redux/reducers/cart";
+import Loader from "../../components/Loader";
+import logoImg from "../../assets/logo.svg";
+import Head from "next/head";
+import { useCallback, useEffect, useRef } from "react";
+import { createGetStr } from "boundless-api-client/utils";
 
 export default function CheckoutPage() {
-	const {id: cartId, cartInited} = useCart();
+	const { id: cartId, cartInited } = useCart();
 	const router = useRouter();
 	const checkoutStarter = useRef<StarterWrapper>();
 
-	const checkoutRef = useCallback((node: HTMLDivElement) => {
-		if (node && cartInited === TCartInited.yes && cartId) {
-			checkoutStarter.current = startCheckout(node, {
-				api: apiClient,
-				cartId,
-				onHide: (element: string, error?: string) => {
-					resetCheckoutState();
-					if (element === 'backToCart') {
-						const query: {error?: string} = {};
-						if (error) {
-							query.error = error;
-						}
+	const checkoutRef = useCallback(
+		(node: HTMLDivElement) => {
+			if (node && cartInited === TCartInited.yes && cartId) {
+				checkoutStarter.current = startCheckout(node, {
+					api: apiClient,
+					cartId,
+					onHide: (element: string, error?: string) => {
+						resetCheckoutState();
+						if (element === "backToCart") {
+							const query: { error?: string } = {};
+							if (error) {
+								query.error = error;
+							}
 
-						router.push(`/cart?${createGetStr(query)}`);
-					} else if (element === 'logo') {
-						router.push('/');
-					} else {
-						console.log('unknown element: ', element);
-					}
-				},
-				onThankYouPage: (data) => {
-					resetCheckoutState();
-					window.location.assign(`/thank-you/${data.orderId}`);
-				},
-				basename: '/checkout',
-				logoSrc: logoImg.src,
-			});
-		}
-	}, [cartInited, cartId]);//eslint-disable-line
+							router.push(`/cart?${createGetStr(query)}`);
+						} else if (element === "logo") {
+							router.push("/");
+						} else {
+							console.log("unknown element: ", element);
+						}
+					},
+					onThankYouPage: (data) => {
+						resetCheckoutState();
+						window.location.assign(`/thank-you/${data.orderId}`);
+					},
+					basename: "/checkout",
+					logoSrc: logoImg.src,
+				});
+			}
+		},
+		[cartInited, cartId]
+	); //eslint-disable-line
 	useEffect(() => {
 		return () => {
 			if (checkoutStarter.current) {
@@ -58,7 +65,7 @@ export default function CheckoutPage() {
 	return (
 		<>
 			<Head>
-				<meta name='robots' content='noindex' />
+				<meta name="robots" content="noindex" />
 			</Head>
 			<div>
 				<div ref={checkoutRef}></div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,49 @@
-import {IProduct} from 'boundless-api-client';
-import {GetServerSideProps, InferGetServerSidePropsType} from 'next';
-import ProductsList from '../components/ProductsList';
-import MainLayout from '../layouts/Main';
-import {apiClient} from '../lib/api';
-import {makeAllMenus} from '../lib/menu';
-import VerticalMenu from '../components/VerticalMenu';
-import {IMenuItem} from '../@types/components';
-import SwiperSlider from '../components/SwiperSlider';
-import cliffImg from '../assets/cliff_1.jpg';
-import cliff2Img from '../assets/cliff_2.jpg';
-import CoverTextInCenter from '../components/CoverTextInCenter';
-import bgImg from '../assets/cover-bg.jpeg';
-import bgPortraitImg from '../assets/cover-bg-portrait.jpg';
-import ProductsSliderByQuery from '../components/ProductsSliderByQuery';
-import {IBasicSettings} from '../@types/settings';
+import { IProduct } from "boundless-api-client";
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
+import ProductsList from "../components/ProductsList";
+import MainLayout from "../layouts/Main";
+import { apiClient } from "../lib/api";
+import { makeAllMenus } from "../lib/menu";
+import VerticalMenu from "../components/VerticalMenu";
+import { IMenuItem } from "../@types/components";
+import SwiperSlider from "../components/SwiperSlider";
+import cliffImg from "../assets/cliff_1.jpg";
+import cliff2Img from "../assets/cliff_2.jpg";
+import CoverTextInCenter from "../components/CoverTextInCenter";
+import bgImg from "../assets/cover-bg.jpeg";
+import bgPortraitImg from "../assets/cover-bg-portrait.jpg";
+import ProductsSliderByQuery from "../components/ProductsSliderByQuery";
+import { IBasicSettings } from "../@types/settings";
 
-export default function IndexPage({products, mainMenu, footerMenu, basicSettings}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+export default function IndexPage({
+	products,
+	mainMenu,
+	footerMenu,
+	basicSettings,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
 	return (
-		<MainLayout mainMenu={mainMenu} footerMenu={footerMenu} basicSettings={basicSettings}>
-			<div className='container'>
+		<MainLayout
+			mainMenu={mainMenu}
+			footerMenu={footerMenu}
+			basicSettings={basicSettings}
+		>
+			<div className="container">
 				<MainPageSlider />
-				<div className='row'>
-					<nav className='col-lg-3 d-none d-lg-block'>
+				<div className="row">
+					<nav className="col-lg-3 d-none d-lg-block">
 						{mainMenu && <VerticalMenu menuList={mainMenu} />}
 					</nav>
-					<div className='col-lg-9 col-md-12'>
-						<h1 className='page-heading page-heading_h1  page-heading_m-h1'>Boundless store</h1>
-						<ProductsList products={products} query={{}}/>
+					<div className="col-lg-9 col-md-12">
+						<h1 className="page-heading page-heading_h1  page-heading_m-h1">
+							Boundless store
+						</h1>
+						<ProductsList products={products} query={{}} />
 					</div>
 				</div>
-				<div className='container'>
-					<h2 className='page-heading page-heading_h1  page-heading_m-h1'>Cover example:</h2>
+				<div className="container">
+					<h2 className="page-heading page-heading_h1  page-heading_m-h1">
+						Who we are
+					</h2>
 				</div>
 			</div>
 			<CoverTextInCenter
@@ -38,41 +51,53 @@ export default function IndexPage({products, mainMenu, footerMenu, basicSettings
 				img={bgImg.src}
 				imgPortrait={bgPortraitImg.src}
 				content={{
-					intro: 'Intro',
-					head: 'Main header',
-					subHead: 'subheader'
+					intro: "Intro",
+					head: "NexTrade",
+					subHead: "Where Convenience Meets Commerce!",
 				}}
 				shadow={{
 					opacity: 0.5,
-					backgroundColor: '#000'
+					backgroundColor: "#000",
 				}}
-				link={'http://google.com'}
+				link={"/"}
 			/>
-			<div className='container'>
-				<h2 className='page-heading page-heading_h1  page-heading_m-h1'>Products carousel:</h2>
+			<div className="container">
+				<h2 className="page-heading page-heading_h1  page-heading_m-h1">
+					Products carousel:
+				</h2>
 				<ProductsSliderByQuery
-					query={{collection: ['main-page'], sort: 'in_collection'}}
-					title={'Collection title'}
-					wrapperClassName='page-block'
+					query={{ collection: ["main-page"], sort: "in_collection" }}
+					title={"Collection title"}
+					wrapperClassName="page-block"
 				/>
 			</div>
 		</MainLayout>
 	);
 }
 
-export const getServerSideProps: GetServerSideProps<IIndexPageProps> = async () => {
-	const categoryTree = await apiClient.catalog.getCategoryTree({menu: 'category'});
-	const {products} = await apiClient.catalog.getProducts({collection: ['main-page'], sort: 'in_collection'});
-	const basicSettings = await apiClient.system.fetchSettings(['system.locale', 'system.currency']) as IBasicSettings;
+export const getServerSideProps: GetServerSideProps<
+	IIndexPageProps
+> = async () => {
+	const categoryTree = await apiClient.catalog.getCategoryTree({
+		menu: "category",
+	});
+	const { products } = await apiClient.catalog.getProducts({
+		collection: ["main-page"],
+		sort: "in_collection",
+	});
+	const basicSettings = (await apiClient.system.fetchSettings([
+		"system.locale",
+		"system.currency",
+	])) as IBasicSettings;
 
-	const menus = makeAllMenus({categoryTree});
+	const menus = makeAllMenus({ categoryTree });
 
 	return {
 		props: {
 			products,
 			basicSettings,
-			...menus
-		}
+			...menus,
+		},
 	};
 };
 
@@ -86,33 +111,33 @@ interface IIndexPageProps {
 function MainPageSlider() {
 	const slides = [
 		{
-			'img': cliffImg.src,
-			'link': '',
-			'caption': 'Three things cannot be long hidden: The Sun, The Moon, and The Truth.',
-			'captionPosition': 'center',
-			'useFilling': true,
-			'fillingColor': '#000000',
-			'fillingOpacity': 0.40
+			img: cliff2Img.src,
+			link: "",
+			caption: "Shop 'til you sparkle and let your style dazzle.",
+			captionPosition: "center",
+			useFilling: true,
+			fillingColor: "#000000",
+			fillingOpacity: 0.4,
 		},
 		{
-			'img': cliff2Img.src,
-			'link': '',
-			'caption': 'Pray not for easy lives, pray to be stronger men.',
-			'captionPosition': null,
-			'useFilling': true,
-			'fillingColor': '#000000',
-			'fillingOpacity': 0.4
-		}
+			img: cliffImg.src,
+			caption: "Shopping is like a box of surprises waiting to be unwrapped.",
+			link: "",
+			captionPosition: null,
+			useFilling: true,
+			fillingColor: "#000000",
+			fillingOpacity: 0.4,
+		},
 	];
 
 	return (
 		<SwiperSlider
 			showPrevNext
 			roundCorners
-			pagination='progressbar'
-			size={'large'}
+			pagination="progressbar"
+			size={"large"}
 			slides={slides}
-			className={'mb-4'}
+			className={"mb-4"}
 		/>
 	);
 }

caadbe05eae4c7a482095974850fa4043d70aef6
Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>
2023-05-28 04:42:40 +0300

    adding auth

--- next.config.js
+++ next.config.js
@@ -1,17 +1,39 @@
-/** @type {import('next').NextConfig} */
+const webpack = require('webpack');
+const {URL} = require('url');
+
+const imgDomains = ['media.boundless-commerce.com'];
+if (process.env.BOUNDLESS_MEDIA_SERVER) {
+	const imgUrl = new URL(process.env.BOUNDLESS_MEDIA_SERVER);
+	imgDomains.push(imgUrl.host);
+}
+
 module.exports = {
-  eslint: {
-    // Disabling on production builds because we're running checks on PRs via GitHub Actions.
-    ignoreDuringBuilds: true
-  },
-  images: {
-    formats: ['image/avif', 'image/webp'],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'cdn.shopify.com',
-        pathname: '/s/files/**'
-      }
-    ]
-  }
+	images: {
+		domains: imgDomains
+	},
+	webpack: (config) => {
+		const defineMap = {};
+
+		[
+			'BOUNDLESS_BASE_URL',
+			'BOUNDLESS_API_BASE_URL',
+			'BOUNDLESS_API_PERMANENT_TOKEN',
+			'BOUNDLESS_S3_PREFIX',
+			'BOUNDLESS_INSTANCE_ID',
+			'BOUNDLESS_PRODUCTS_IMAGE_PROPORTION',
+			'BOUNDLESS_MEDIA_SERVER'
+		].forEach((key) => defineMap[`process.env.${key}`] = JSON.stringify(process.env[key]));
+
+		config.plugins.push(
+			new webpack.DefinePlugin(defineMap)
+		);
+
+		return config;
+	},
 };
+
+// const withBundleAnalyzer = require('@next/bundle-analyzer')({
+//   enabled: process.env.ANALYZE === 'true'
+// });
+
+// module.exports = withBundleAnalyzer({});
